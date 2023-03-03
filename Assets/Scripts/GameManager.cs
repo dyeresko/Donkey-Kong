3c21189e1977b1718e0e3bc1553a86fcb8e9e078
Dyeresko <denis.yeresko@gmail.com>
2023-03-03 16:15:00 +0200

    Donkey Kong

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int lives;
+    private int score;
+
+    private void Start()
+    {
+        NewGame();
+    }
+    private void NewGame()
+    {
+        lives = 3;
+        score = 0;
+    }
+    public void LevelComplete()
+    {
+        score += 1000;
+    }
+    public void LevelFailed()
+    {
+        lives--;
+        if (lives <= 0)
+        {
+            NewGame();
+        }
+        else
+        {
+
+        }
+    }
+}
